2f29e37a0833b410673abdf5d6227f0d0e739e86
TXHENG-MSI\user <off.txh0607@gmail.com>
2020-11-03 21:53:49 +0800

    Feature: first start on git + nodejs

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+app.listen(3000);
+app.use('/node_modules',express.static('node_modules'));
+app.set('view engine', 'ejs');
+
+app.get('/',function(req, res){
+    res.render('user/index');
+});
+
+app.get('/profile/:name', function(req, res){
+    res.render('/user/index',{person: req.params.name});
+});

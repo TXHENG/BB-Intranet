b5608887d5bc5d47187114a790ecc1e526da3c38
TXHENG-MSI\user <off.txh0607@gmail.com>
2020-11-04 00:55:59 +0800

    Improvement: create sidebar without design.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 app.listen(3000);
 app.use('/node_modules',express.static('node_modules'));
+app.use('/resources',express.static('resources'));
 app.set('view engine', 'ejs');
 
 app.get('/',function(req, res){
